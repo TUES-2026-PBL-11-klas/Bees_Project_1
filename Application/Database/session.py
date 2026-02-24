e5d34b7091a20596382551d9726a02cc519a2bc0
Radoslav <radoslav.s.slavov@gmail.com>
2026-02-24 16:30:58 +0200

    Removed a \n in base.py, Modified session.py using 'SettingsConfigDict' to upgrade Pydantic v2 and broke down the database url so that it is safer

--- Application/Database/session.py
+++ Application/Database/session.py
@@ -1,15 +1,31 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent.parent
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str
+    postgres_port: int
+    
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql://{self.postgres_user}:"
+            f"{self.postgres_password}@"
+            f"{self.postgres_host}:"
+            f"{self.postgres_port}/"
+            f"{self.postgres_db}"
+        )
 
-    class Config:
-        env_file = str(BASE_DIR / ".env")
+    model_config = SettingsConfigDict(
+        BASE_DIR / ".env",
+        extra="ignore"
+    )
 
 settings = Settings()
 
